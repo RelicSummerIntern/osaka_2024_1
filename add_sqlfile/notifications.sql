ddc7f3e3d371e552e2f73b1da6823c972ce67c61
yamana <cdhura46@gmail.com>
2024-08-20 16:49:32 +0900

    sqlファイルを追加

--- add_sqlfile/notifications.sql
+++ add_sqlfile/notifications.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: mysql
--- 生成日時: 2024 年 8 月 20 日 07:15
+-- 生成日時: 2024 年 8 月 20 日 07:48
 -- サーバのバージョン： 8.0.32
 -- PHP のバージョン: 8.2.8
 
@@ -37,6 +37,16 @@ CREATE TABLE `notifications` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
+-- テーブルのデータのダンプ `notifications`
+--
+
+INSERT INTO `notifications` (`notification_id`, `title`, `body`, `created_at`, `updated_at`, `last_editor_id`) VALUES
+(2, '新店舗がオープンしました！', '本文2', '2024-08-20 07:46:04', '2024-08-20 07:46:04', 'daijiro'),
+(3, '七夕祭りが開催されました！', '七夕祭りが盛大に開催され、色とりどりの笹飾りが街を彩りました。多くの人々が短冊に願いを書き、伝統的な風習を楽しみました。地域の活気溢れるイベントとなりました。', '2024-08-20 07:46:56', '2024-08-20 07:46:56', 'yutaro'),
+(4, '夏祭りが開催されました！', '本文4', '2024-08-20 07:47:39', '2024-08-20 07:47:39', 'yutaro'),
+(5, '秋祭りが開催されました！', '本文5', '2024-08-20 07:48:06', '2024-08-20 07:48:06', 'takuma');
+
+--
 -- ダンプしたテーブルのインデックス
 --
 
@@ -54,7 +64,7 @@ ALTER TABLE `notifications`
 -- テーブルの AUTO_INCREMENT `notifications`
 --
 ALTER TABLE `notifications`
-  MODIFY `notification_id` bigint NOT NULL AUTO_INCREMENT COMMENT '記事ID';
+  MODIFY `notification_id` bigint NOT NULL AUTO_INCREMENT COMMENT '記事ID', AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
