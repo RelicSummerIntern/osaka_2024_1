9fcc4ea10ad318ff47a2f435690eb44d7d89d001
SYutaroT <sachyand0515sachy@gmail.com>
2024-08-20 16:17:00 +0900

    MAPの追加

---
+++ resources/views/map/index.blade.php
@@ -0,0 +1,24 @@
+<html>
+
+<head>
+    <title>Hello world</title>
+    <style>
+        body {
+            font-size: 16px;
+            color: #999;
+        }
+
+        h1 {
+            font-size: 120px;
+            text-align: right;
+            color: #fafafa;
+            margin: -50px 0px -120px 0px;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Index</h1>
+</body>
+
+</html>
