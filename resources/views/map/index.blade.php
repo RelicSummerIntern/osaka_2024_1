3cf262909f6c6ca44fe5fab522c84fd7049ece8f
SYutaroT <sachyand0515sachy@gmail.com>
2024-08-21 12:35:00 +0900

    CSSを改良

--- resources/views/map/index.blade.php
+++ resources/views/map/index.blade.php
@@ -1,21 +1,15 @@
-<html>
 
-<head>
-    <title>zimo_map</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-</head>
+@extends('layouts.nav')
 
-<body>
-    <h1 class="tytle">○○商店街　簡単MAP</h1>
+@section('title', 'マップページ')
+
+@section('content')
+    <h1 class="tytle">○○商店街 簡単MAP</h1>
     <img class="center_img" src="{{ asset('img/ALLmap.png') }}" usemap="#storemap" alt="Store Map" width=1200>
 
-<map name="storemap">
-<area shape="rect" coords="622,76,805,266" href="{{ route('map.hamburger') }}" alt="Hamburger Shop">
-<area shape="rect" coords="88,146,248,303" href="{{ route('map.shuttered1') }}" alt="shuttered Shop1">
+    <map name="storemap">
+        <area shape="rect" coords="622,76,805,266" href="{{ route('map.hamburger') }}" alt="Hamburger Shop">
+        <area shape="rect" coords="88,146,248,303" href="{{ route('map.shuttered1') }}" alt="shuttered Shop1">
 
-
-</map>
-
-</body>
-
-</html>
+    </map>
+@endsection
