e8bee1ad0ec101e49ed8e0d685fcb5788191d1b3
yamana <cdhura46@gmail.com>
2024-08-21 14:23:40 +0900

    前後の記事を表示

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -12,9 +12,23 @@ class NotificationsController extends Controller
         $notifications = Notification::all();
         return view('notifications.index', ['notifications'=>$notifications]);
     }
-    public function show($notification_id){
-        // $notification = Notification::findOrFail($notification_id);
-        $notification = Notification::where('notification_id', $notification_id)->firstOrFail();
-        return view('notifications.show', ['notification'=>$notification]);
+    public function show($id){
+        // $notification = Notification::findOrFail($id);
+        
+        // 現在のレコードを取得
+        $notification = Notification::where('notification_id', $id)->firstOrFail();
+
+        // 1つ前のレコードを取得
+        $prevNotification = Notification::where('notification_id', '<', $id) //$notification_idより小さいカラムnotification_idを持つレコードを取ってくる
+        ->orderBy('notification_id', 'desc') // desc：降順に並べる
+        ->first(); // 最初のレコードを取ってくる
+
+        // 1つ後のレコードを取得
+        $nextNotification = Notification::where('notification_id', '>', $id)
+        ->orderBy('notification_id', 'asc') // asc：昇順に並べる
+        ->first(); // 最初のレコードを取ってくる
+
+        return view('notifications.show', ['prevNotification'=>$prevNotification, 'notification'=>$notification, 'nextNotification'=>$nextNotification]);
+
     }
 }
--- resources/views/notifications/show.blade.php
+++ resources/views/notifications/show.blade.php
@@ -4,6 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $notification->title }}</title>
+    <style>
+        .disabled-link {
+            color: #cccccc; /* 色を薄くする */
+            pointer-events: none; /* クリックできないようにする */
+            text-decoration: none; /* アンダーラインを削除する */
+        }
+    </style>
 </head>
 <body>
     <x-link-on-the-top/>
@@ -11,5 +18,22 @@
     <div>作成日：{{ $notification->created_at }}</div>
     <div>最終更新日：{{ $notification->updated_at }}</div>
     <div>{{ $notification->body }}</div>
+
+    <hr>
+    @if($prevNotification)
+        <a href="{{ route('notifications.show', ['id'=> $prevNotification->notification_id]) }}">
+            前の記事
+        </a>
+    @else
+        <span class="disabled-link">前の記事</span>
+    @endif
+    &nbsp;&nbsp;&nbsp;
+    @if($nextNotification)
+        <a href="{{ route('notifications.show', ['id'=> $nextNotification->notification_id]) }}">
+            次の記事
+        </a>
+    @else
+        <span class="disabled-link">次の記事</span>
+    @endif
 </body>
 </html>
