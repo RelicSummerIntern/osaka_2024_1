6193f9f0d03ff35f4d4ee5d7a8464582a39f7940
yamana <cdhura46@gmail.com>
2024-08-22 19:10:09 +0900

    yamana2

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -6,13 +6,13 @@
 
 @section('content')
 <h1 class="tytle">商店街からのお知らせ</h1>
-    <ul class="news-list">
-        @foreach($notifications as $notification)
-            <li class="item"><a href='http://localhost/notifications/{{ $notification -> notification_id }}'>
-            <p class="date">{{ $notification->created_at }}</p>
-            <p class="category"><span>お知らせ</span></p>
-            <p class="title">{{ $notification->title }}</p>
-            </a></li>
-        @endforeach
-    </ul>
-    @endsection
+<ul class="news-list">
+    @foreach($notifications as $notification)
+        <li class="item"><a href='http://localhost/notifications/{{ $notification -> notification_id }}'>
+        <p class="date">{{ $notification->created_at }}</p>
+        <p class="category"><span>お知らせ</span></p>
+        <p class="title">{{ $notification->title }}</p>
+        </a></li>
+    @endforeach
+</ul>
+@endsection
