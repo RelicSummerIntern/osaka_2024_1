b05b3dc4dce108ab78b1a39fea14d49f49909703
yamana <d.yamana0511@gmail.com>
2024-08-23 08:03:08 +0900

    空き店舗とお知らせ記事を修正

--- resources/views/notifications/show.blade.php
+++ resources/views/notifications/show.blade.php
@@ -13,27 +13,25 @@
     <hr>
     <div class="container">
         <div class="body">
-            @if($prevNotification)
-                <a href="{{ route('notifications.show', ['id'=> $prevNotification->notification_id]) }}">
-                    前の記事
+            @if($nextNotification)
+                <a href="{{ route('notifications.show', ['id'=> $nextNotification->notification_id]) }}">
+                    1つ後の記事へ
                 </a>
             @else
-            <span class="disabled-link">前の記事</span>
+                <span class="disabled-link">1つ後の記事へ</span>
             @endif
         </div>
-
         <!-- 一覧に戻るボタン -->
         <div class="body">
             <a href="{{ route('notifications.index') }}">一覧に戻る</a>
         </div>
-
         <div class="body">
-            @if($nextNotification)
-                <a href="{{ route('notifications.show', ['id'=> $nextNotification->notification_id]) }}">
-                    次の記事
+            @if($prevNotification)
+                <a href="{{ route('notifications.show', ['id'=> $prevNotification->notification_id]) }}">
+                    1つ前の記事へ
                 </a>
             @else
-                <span class="disabled-link">次の記事</span>
+            <span class="disabled-link">1つ前の記事へ</span>
             @endif
         </div>
     </div>
