96baf1d55276fed8063058e05462765f9144c42c
SYutaroT <sachyand0515sachy@gmail.com>
2024-08-22 13:47:56 +0900

    "マップを変更"

--- resources/views/map/index.blade.php
+++ resources/views/map/index.blade.php
@@ -3,15 +3,18 @@
 @section('title', 'マップページ')
 
 @section('content')
-<h1 class="tytle">○○商店街 イラストMAP</h1>
+<h1 class="tytle">西新道錦会商店街 イラストMAP</h1>
 <p class="invite">気になるお店のアイコンをクリック！
     <br>各お店の詳細ページに飛べます
 </p>
-<img class="center_img" src="{{ asset('img/ALLmap.png') }}" usemap="#storemap" alt="Store Map" width=1200>
+<img class="center_img" src="{{ asset('img/arcade2.png') }}" usemap="#storemap" alt="Store Map" width=100%>
 <map name="storemap">
-    <area shape="rect" coords="622,76,805,266" href="{{ route('map.hamburger') }}" alt="Hamburger Shop" data-tooltip="佐々木バーガー #ハンバーガー　#ランチ">
-    <area shape="rect" coords="88,146,248,303" href="{{ route('map.shuttered1') }}" alt="shuttered Shop1" data-tooltip="シャッター店 #新店舗募集中">
-        
+    <area shape="rect" coords="1005,277,8051230,467" href="{{ route('map.hamburger') }}" alt="Murase" data-tooltip="お肉屋～村瀬～ #お肉　#コロッケ">
+    <area shape="rect" coords="770,140,987,340" href="{{ route('map.shuttered1') }}" alt="shuttered Shop1" data-tooltip="シャッター店 #新店舗募集中">
+    <area shape="rect" coords="505,0,738,197" href="{{ route('map.shuttered1') }}" alt="shuttered Shop1" data-tooltip="花屋～新島花屋～ #きれいなお花売ってます">
+    <area shape="rect" coords="90,236,313,422" href="{{ route('map.shuttered1') }}" alt="shuttered Shop1" data-tooltip="八百屋～立花～ #産地直送">
+    <area shape="rect" coords="328,391,526,583" href="{{ route('map.shuttered1') }}" alt="shuttered Shop1" data-tooltip="魚屋～錦魚～ #〆鯖あります">
+    <area shape="rect" coords="591,508,811,703" href="{{ route('map.shuttered1') }}" alt="shuttered Shop1" data-tooltip="シャッター店 #新店舗募集中">
 </map>
 
 @endsection
